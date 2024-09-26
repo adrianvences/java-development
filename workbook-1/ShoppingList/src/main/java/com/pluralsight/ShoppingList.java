820ae8f23af2f74044623ed00698da58f21eaf99
adrianvences9820 <adrianvences9820@gmail.com>
2024-09-26 10:12:11 -0400

    testing commit through intelj

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -2,6 +2,6 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main (String[] args) {
-        System.out.println("ShoppingList!\nEggs\nHam\nBread\nMayo\nLettuce\nTomatoes\nOnions\nHamburger Patties\nHamburger Buns\nKetchup");
+        System.out.println("TestShoppingList!\nEggs\nHam\nBread\nMayo\nLettuce\nTomatoes\nOnions\nHamburger Patties\nHamburger Buns\nKetchup");
     }
 }
