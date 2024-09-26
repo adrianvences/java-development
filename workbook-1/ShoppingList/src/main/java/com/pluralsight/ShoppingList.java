9d3b20f9b9db038acdd9657f1898630954a67219
adrianvences9820 <adrianvences9820@gmail.com>
2024-09-26 10:01:33 -0400

    completed ShoppingList app

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -4,4 +4,4 @@ public class ShoppingList {
     public static void main (String[] args) {
         System.out.println("ShoppingList!\nEggs\nHam\nBread\nMayo\nLettuce\nTomatoes\nOnions\nHamburger Patties\nHamburger Buns\nKetchup");
     }
-} 
+}
